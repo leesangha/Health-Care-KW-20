1f5c49600a6cba5aafc45f7715fc59ec6e315d3b
이승철 <thankfather@naver.com>
2020-03-02 17:32:54 +0900

    NavigationBar를 일반적인 이름인 Header로 수정해주었습니다.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import SignUp from './SignUp';
 import Login from './Login';
 import './App.css';
 import { Route, Link, Switch } from 'react-router-dom';
-import NavigationBar from "./components/NavigationBar";
+import Header from "./components/Header";
 import Home from "./routes/Home";
 
 function App() {
   return (
     <div>
-      <NavigationBar/>
+      <Header/>
       <Switch>
         <Route path = "/" component = {Home} exact/>
         <Route path = "/SignUp" component ={SignUp}/>
