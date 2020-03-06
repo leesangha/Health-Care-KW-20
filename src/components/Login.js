b8fe2e0402bd49944ac75d34bb4836b79aed274c
이승철 <thankfather@naver.com>
2020-03-06 20:46:37 +0900

    포맷에 맞게 띄어쓰기를 수정해주었습니다.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,73 +1,79 @@
-import React,{useState,useCallback} from 'react';
-function Login(){
+import React, { useState, useCallback } from 'react';
+import './Login.scss';
+
+function Login({setLog}) {
     const [inputs,setInputs] = useState({
         id:'',
         password:''
     });
     const {id, password} = inputs;
 
-    const onChange = useCallback(e=>{
-        const {name,value} = e.target;
-        setInputs({
-            ...inputs,
-            [name]:value
-        });
-    },[inputs]
+    const onChange = useCallback(e => {
+          const {name,value} = e.target;
+          setInputs({
+              ...inputs,
+              [name]:value
+          });
+      },[inputs]
     );
 
-    const isSuccess = () =>{
+    const isSuccess = () => {
         setInputs({
-        id:'',
-        password:''
+            id:'',
+            password:''
         })
-    }
+    };
 
-    const onClick = ()=>{
+    const onClick = () => {
         fetch('/process/login',{method: 'POST',body:JSON.stringify(inputs),
-        headers:{
-            "Content-Type":"application/json",
-            "Accept":"application/json"
-        }})
-        .then(res => res.json())
-        .then(data => {
-            //Login Fail
-            if(data.err ==='error')
-               {
-                console.log('login fail');
-               }
-            else{
-            localStorage.setItem('info',JSON.stringify(data));
-            console.log(localStorage.getItem('info'));
-            isSuccess();
-            }
-            
-        });
-    }
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":"application/json"
+            }})
+          .then(res => res.json())
+          .then(data => {
+              //Login Fail
+              if(data.err ==='error')
+              {
+                  console.log('login fail');
+              }
+              else{
+                  localStorage.setItem('info',JSON.stringify(data));
+                  console.log(localStorage.getItem('info'));
+                  isSuccess();
+              }
+
+          });
+    };
+
     return (
-        <div>
-            <p>로그인 페이지 입니다.</p>
-            <p>
-                서식 
-                <ul>
-                   
-                    <li>ID</li>
-                    <input name = "id"
-                    placeholder = "id"
-                    onChange = {onChange}
-                    value = {id}
-                    />
-                    <br/>
-                    <li>PASSWORD</li>
-                     <input name = "password"
-                    placeholder = "password"
-                    onChange = {onChange}
-                    value = {password}
-                    />
-                    <br/>
-                </ul>
-            </p>
-            <button onClick = {onClick} > 제출</button>
+      <div className="login-page">
+        <div className="introduce">
         </div>
+        <aside className='login-container'>
+          <p>로그인 페이지 입니다.</p>
+          <p>
+            서식
+            <ul>
+              <li>ID</li>
+              <input name = "id"
+                     placeholder = "id"
+                     onChange = {onChange}
+                     value = {id}
+              />
+              <br/>
+              <li>PASSWORD</li>
+              <input name = "password"
+                     placeholder = "password"
+                     onChange = {onChange}
+                     value = {password}
+              />
+              <br/>
+            </ul>
+          </p>
+          <button onClick = {onClick} > 제출</button>
+        </aside>
+      </div>
     )
 }
 export default Login;
