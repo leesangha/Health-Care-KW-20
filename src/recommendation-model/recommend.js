0c5ea9188aa87fc7f304c344981a6a4dd679afd6
이승철 <thankfather@naver.com>
2020-03-07 15:24:59 +0900

    이차원 배열을 다른 모듈로부터 받아서 실행되도록 변경했습니다.

--- src/recommendation-model/recommend.js
+++ src/recommendation-model/recommend.js
@@ -1,34 +1,18 @@
 const { PythonShell } = require('python-shell');
 
-function getAllPreferences() {
-  const list = [[0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [0, 0, 0, 0, 0, 0, 0, 1, 0, 4, 0],
-                [0, 3, 4, 0, 3, 0, 0, 2, 2, 0, 0],
-                [0, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 5, 0, 0, 5, 0],
-                [0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 5],
-                [0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 4],
-                [0, 0, 0, 0, 0, 0, 5, 0, 0, 5, 0],
-                [0, 0, 0, 3, 0, 0, 0, 0, 4, 5, 0]];
-  return list;
-}
-
 function getListShape(list) {
   return [list.length, list[0].length]
 }
 
-function recommend() {
-  const list = getAllPreferences();
-  const shape = getListShape(list);
-  const userNumber = 3;
+function recommend(preferenceList, userNumber) {
+  const shape = getListShape(preferenceList);
 
   const options = {
     mode: 'text',
     pythonPath: '/opt/anaconda3/envs/tf1/bin/python3',
     pythonOptions: ['-u'],
     scriptPath: __dirname,
-    args: [list, shape, userNumber]
+    args: [preferenceList, shape, userNumber]
   };
 
   PythonShell.run('recommend_test01.py',
