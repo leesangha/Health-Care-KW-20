9283f0539652063c65eb2d68340024e7bd45f588
이승철 <thankfather@naver.com>
2020-03-18 19:36:43 +0900

    Home에 있는 이미지 링크를 가져오는 부분을 Recommendation.js로 옮겼습니다.

--- src/components/Recommendation.js
+++ src/components/Recommendation.js
@@ -2,23 +2,32 @@ import React, { useEffect,useState } from "react";
 import "./Recommendation.scss"
 import Food from "./Food";
 
-function Recommendation({foodImageList}) {
-  const UserInfo = sessionStorage.getItem('info');
-  const [user_no,setUser_no] = useState();
-  useEffect(() => {
-      setUser_no(JSON.parse(UserInfo)[0].user_no) 
-  }
-  )
+function getFoodImage(foodArray) {
+  // 서버에서 이미지를 가져오는 부분
+  let list = [
+    '/images/0.jpeg', "/images/1.jpg", "/images/2.jpg", "/images/3.jpg",
+    '/images/4.jpg', "/images/5.jpg", '/images/6.jpg'
+  ];
+  return list;
+}
 
-  //console.log('recommendation :' + user_no[0].user_no);
+function Recommendation() {
+  const UserInfo = sessionStorage.getItem('info');
+  const foodImageList = getFoodImage();
+
+  const [user_no,setUser_no] = useState();
+
+  useEffect(() => {
+      setUser_no(JSON.parse(UserInfo)[0].user_no)
+    }
+  , [UserInfo]);
+
   return (
     <article className="recommendation">
       <h1>이런 음식 어때요?</h1>
-      {foodImageList.map(imageSrc => {
-        return (
-          <Food key={imageSrc} imageSrc={imageSrc} num = {user_no}/>
-        );
-      })}
+      {foodImageList.map(imageSrc => (
+        <Food key={imageSrc} imageSrc={imageSrc} num={user_no}/>
+      ))}
     </article>
   );
 }
