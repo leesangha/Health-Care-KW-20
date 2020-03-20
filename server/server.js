33ee6498f5de2c9218945edda42e10f9681466d6
이승철 <thankfather@naver.com>
2020-03-20 22:49:23 +0900

    선호도를 가져오는 부분을 Router로 처리해주었습니다.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const path = require('path');
 const router = require('./routes/router');
-const db = require('./dbconnection');
 const loginRouter = require('./routes/login');
 const addUserRouter = require('./routes/addUser');
 const hateRouter = require('./routes/hate');
-const { getNutritionRouter, getIntakeRouter } = require('./routes/getUserData');
+const { getNutritionRouter, getIntakeRouter, getUserPreference } = require('./routes/getUserData');
 
 const app = express();
 app.use(express.json());
@@ -18,7 +17,8 @@ app.post("/addUser", addUserRouter);
 app.post("/process/login", loginRouter);
 app.post("/hate", hateRouter);
 app.post("/getNutrition", getNutritionRouter);
-app.post("/getIntake",getIntakeRouter);
+app.post("/getIntake", getIntakeRouter);
+app.post("/getUserPreference", getUserPreference);
 
 app.use("/",router);
 
