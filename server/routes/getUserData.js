54fa556ca61754fab5603c71a254a4313de820ac
이승철 <thankfather@naver.com>
2020-03-22 16:36:21 +0900

    자바스크립트 문법을 사용해서 유저수와 선호도 배열을 가져옵니다.

--- server/routes/getUserData.js
+++ server/routes/getUserData.js
@@ -22,40 +22,19 @@ function getIntakeRouter(req, res) {
 }
 
 function getUserPreference(req, res) {
-  db.query("select * from user_preference", async (e,r) => {
-    if(r.recordset === undefined || e)
+  db.query("select * from user_preference", async (err, rows) => {
+    const usersPreferences = rows.recordset;
+
+    if(usersPreferences === undefined || err)
       res.send({err:'error'});
     else {
-      let max_user_no = 0;
-      let num = 0;
-      //유저수 체크
-      while(true) {
-        try {
-          const a = r.recordset[num]['음식_0'];
-          num +=1;
-        }
-        catch(e){
-          max_user_no = num;
-          break;
-        }
-      }
-      console.log('max_user의 수 ' + max_user_no);
+      const totalUsers = usersPreferences.length;
+      console.log('Total users : ' + totalUsers);
 
-      let preference = Array(max_user_no).fill(null).map(() => Array());
-      console.log(preference);
-      let i = 0;
-      while(true) {
-        let u_no;
-        try {
-          for(let j = 0; j< 515; j++) {
-            u_no = i;
-            preference[u_no][j] = r.recordset[i]['음식_'+j];
-          }
-          i+=1;
-        } catch(e){
-          break;
-        }
-      }
+      let preference = usersPreferences.map(object => {
+        const values = Object.values(object);
+        return values.slice(1, values.length);
+      });
 
       // 선호도 모델 계산되고 정렬되어 반환됨.
       preference = await predictPreference(preference);
