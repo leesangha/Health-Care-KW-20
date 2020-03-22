c0643d322865a19bbd2026cf3f88ec46bbda7696
leesangha <37141545+leesangha@users.noreply.github.com>
2020-03-23 01:36:52 +0900

    Update Recommendation.js

--- src/components/Recommendation.js
+++ src/components/Recommendation.js
@@ -8,36 +8,32 @@ function getFoodImage(foodArray) {
     '/images/0.jpeg', "/images/1.jpg", "/images/2.jpg", "/images/3.jpg",
     '/images/4.jpg', "/images/5.jpg", '/images/6.jpg'
   ];
+
+
+
   return list;
 }
 
 function Recommendation() {
   const foodImageList = getFoodImage();
-
-  const [user_no, setUser_no] = useState();
-  const [foodList, setFoodList] = useState(null);
-
+  const [user_no, setUserNo] = useState();
+  const UserInfo = sessionStorage.getItem('info');
   useEffect(() => {
     if(UserInfo){
       setUserNo(JSON.parse(UserInfo)[0].user_no);
-      //console.log('user_no: ' + user_no);
     }
     else
       console.log('fail');
   },[user_no]);
 
-  useEffect(() => {
-    fetch('/getUserPreference', {method: "POST"})
-      .then(req => req.json())
-      .then(data => setFoodList(data));
-  }, []);
-
   return (
     <article className="recommendation">
       <h1>이런 음식 어때요?</h1>
-      {foodImageList.map(imageSrc => (
-        <Food key={imageSrc} imageSrc={imageSrc} num={user_no}/>
-      ))}
+      {foodImageList.map(imageSrc => {
+        return (
+          <Food key={imageSrc} imageSrc={imageSrc} num = {user_no}/>
+        );
+      })}
     </article>
   );
 }
