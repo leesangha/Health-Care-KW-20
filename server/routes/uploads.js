d39ef5927f00ebe249006f27023584a3cae37552
이승철 <thankfather@naver.com>
2020-03-25 22:18:37 +0900

    파일 업로드 구현했습니다.

--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -1,31 +1,26 @@
 const express = require('express');
-const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
+const moment = require('moment');
+const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: (req, res, callback) => {
+    console.log(req.body.img);
     const id = req.body.id;
-    const userDir = path.join(__dirname, 'uploads', id);
+    const userDir = path.resolve('server', 'uploads', id);
 
     if(!fs.existsSync(userDir)) {
       fs.mkdirSync(userDir);
     }
-    callback(null, `uploads/${id}`);
+    callback(null, userDir);
   },
   filename: (req, file, callback) => {
     const extension = path.extname(file.originalname);
     const basename = path.basename(file.originalname, extension);
 
-    let now = new Date();
-    const timezoneOffset = now.getTimezoneOffset();
-
-    now = new Date(now.getTime() + timezoneOffset * 60000 * (-1));
-    now = now.toISOString().split('T')[0];
-    now = now.split('-');
-    now = now.join('');
-
+    const now = moment().format('YYMMDD-hh:mm:ss');
     callback(null, basename + '-' + now + extension);
   }
 });
@@ -38,12 +33,16 @@ const upload = multer({
   }
 });
 
+router.use((req, res, next) => {
+  console.log('uploads 미들웨어 호출됨.');
+
+  next();
+});
+
 router.post('/', upload.single('img'), (req, res) => {
-  const file = req.file;
-  if(file === undefined)
-    console.log('There is no file');
-  else
-    console.log(file);
+  console.log(req.body);
+  console.log(req.file);
+  console.log(req.file.filename);
 });
 
 module.exports = router;
