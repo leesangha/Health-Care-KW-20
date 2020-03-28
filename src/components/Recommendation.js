125f01a04a786f2a413996c4c700d2ff362451ba
이승철 <thankfather@naver.com>
2020-03-28 23:07:40 +0900

    로딩 spinner 추가, 서버에서 이미지 가져오는 함수 추가

--- src/components/Recommendation.js
+++ src/components/Recommendation.js
@@ -3,23 +3,28 @@ import "./Recommendation.scss"
 import Food from "./Food";
 import {PacmanLoader} from "react-spinners";
 
-function getFoodImage(foodArray) {
-  // 서버에서 이미지를 가져오는 부분
-  let list = [
-    '/images/0.jpeg', "/images/1.jpg", "/images/2.jpg", "/images/3.jpg",
-    '/images/4.jpg', "/images/5.jpg", '/images/6.jpg'
-  ];
-  return list;
+function getFoodImage(foodList) {
+  if(foodList === null || foodList === undefined)
+    return;
+
+  let imgSrcList = [];
+  for(let i = 0; i < 6; ++i) {
+    const imgSrc = `http://localhost:4002/images/${foodList[i]}.png`;
+    imgSrcList.push(imgSrc);
+  }
+  console.log(imgSrcList);
+
+  return imgSrcList.map(src => {
+    return <Food key={src} imageSrc={src} />
+  });
 }
 
 function Recommendation() {
-  const foodImageList = getFoodImage();
   const [foodList, setFoodList] = useState(null);
 
   useEffect(() => {
     const userInfo = JSON.parse(sessionStorage.getItem('info'));
     const userNumber = userInfo[0].user_no;
-    console.log(userNumber);
 
     fetch('/userData/preference', {
         method: "POST",
@@ -32,27 +37,28 @@ function Recommendation() {
     )
       .then(req => req.json())
       .then(data => {
-        const predictedFoodList = data.pref.map(obj => obj.food_no);
+        let predictedFoodList = [];
+        console.log(data.pref)
+        data.pref.forEach(obj => predictedFoodList.push(obj.food_no));
         setFoodList(predictedFoodList);
       });
   }, []);
 
 
-  return foodList === null ? (
-    <div className="loader">
-      <PacmanLoader
-        size={20}
-        color={'#646464'}
-      />
-    </div>
-  ) : (
-    <article className="recommendation">
-      <h1>이런 음식 어때요? </h1>
-      {foodImageList.map(imageSrc => (
-        <Food key={imageSrc} imageSrc={imageSrc} />
-      ))}
-    </article>
-  );
+  return foodList === null || foodList === undefined
+    ? (
+      <div className="loader">
+        <PacmanLoader
+          size={20}
+          color={'#646464'}
+        />
+      </div>
+    ) : (
+      <article className="recommendation">
+        <h1>이런 음식 어때요? </h1>
+        {getFoodImage(foodList)}
+      </article>
+    );
 }
 
 export default Recommendation;
